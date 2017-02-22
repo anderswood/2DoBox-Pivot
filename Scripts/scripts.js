5871c7acbb4add3e40fefb15f42ae295733ffd68
kamos1 <keji.amos@gmail.com>
2017-02-21 17:25:10 -0700

    refactor: add closest replace idea with card restructure code

--- Scripts/scripts.js
+++ Scripts/scripts.js
@@ -1,36 +1,114 @@
 var data = [];
 
-$(document).ready(function() {
-    getIdea();
-    printIdea();
+getCard();
+printCard();
+
+$("#title-input, #content-input").on("keyup", disableEnter);
+
+$("#submit").on('click', function(e) {
+  e.preventDefault();
+  var storeCardTitle = $('#title-input').val();
+  var storeCardContent = $('#content-input').val();
+  var card = new Card(storeCardTitle, storeCardContent);
+  data.unshift(card);
+  storeCard();
+  printCard();
+  clearInput();
+  disableEnter();
 })
 
-function Card(storeIdeaTitle, storeIdeaContent) {
-    this.title = storeIdeaTitle;
-    this.body = storeIdeaContent;
+$("#card-section").on('click', '.upvote', function() {
+  var qualityVar = $(this).siblings(".quality").text();
+  if ($(this).siblings(".quality").text() === "swill") {
+    $(this).siblings(".quality").text("plausible");
+    qualityVar = "plausible";
+    editQuality(this, qualityVar);
+  } else if ($(this).siblings(".quality").text() === "plausible") {
+    $(this).siblings(".quality").text("genius");
+    qualityVar = "genius"
+    editQuality(this, qualityVar);
+  } else if ($(this).siblings(".quality").text() === "genius") {
+    qualityVar = "genius";
+  }
+});
+
+$("#card-section").on('click', '.downvote', function() {
+  var qualityVar = $(this).siblings(".quality").text();
+  if ($(this).siblings(".quality").text() === "genius") {
+    $(this).siblings(".quality").text("plausible");
+    qualityVar = "plausible";
+    editQuality(this, qualityVar);
+  } else if ($(this).siblings(".quality").text() === "plausible") {
+    $(this).siblings(".quality").text("swill");
+    qualityVar = "swill";
+    editQuality(this, qualityVar);
+  } else if ($(this).siblings(".quality").text() === "plausible") {
+    qualityVar = "swill";
+  }
+});
+
+$('#card-section').on('blur', '.entry-title', function() {
+  var newTitleText = $(this).text();
+  editTitleText(this, newTitleText);
+});
+
+$('#card-section').on('blur', '.entry-body', function() {
+  var newBodyText = $(this).text();
+  editBodyText(this, newBodyText);
+});
+
+
+$("#card-section").on('click', '.delete', function() {
+  var idOfRemoved = $(this).closest('.new-idea').attr('id');
+  console.log(idOfRemoved);
+  deleteCard(this, idOfRemoved);
+  $(this).closest('.new-idea').remove();
+});
+
+
+$('#search').on('keyup', function() {
+  var searchInput = $('#search').val();
+  var re = new RegExp(searchInput, 'igm');
+  $('.new-idea').each(function() {
+    var title = $(this).find(".entry-title").text();
+    var body = $(this).find("article p").text();
+    var match = (title.match(re) || body.match(re));
+    if (!match) {
+      $(this).hide();
+    } else {
+      $(this).show();
+    }
+  })
+});
+
+$("#title-input, #content-input").on("keyup", disableEnter);
+
+function Card(storeCardTitle, storeCardContent) {
+    this.title = storeCardTitle;
+    this.body = storeCardContent;
     this.quality = "swill";
     this.id = Date.now();
 }
 
-function storeIdea() {
+function storeCard() {
     var stringData = JSON.stringify(data);
     localStorage.setItem("Data Item", stringData);
 }
 
-function getIdea() {
+function getCard() {
     var storedData = localStorage.getItem("Data Item") || '[]';
     var parsedData = JSON.parse(storedData);
     data = parsedData;
 }
 
-function printIdea() {
-    $("#card-section").html('');
-    data.forEach(function(object) {
-        $("#card-section").append(`
-			<div id="${object.id}" class="new-idea">
+function printCard() {
+  $("#card-section").html('');
+  data.forEach(function(object) {
+    $("#card-section").append(
+      `<div id="${object.id}" class="new-idea">
 				<header>
 					<h1 class="entry-title" contenteditable='true'>${object.title}</h1>
-					<button class="clear"></button>
+					<button class="delete"></button>
 				</header>
 				<article>
 					<p class='entry-body' contenteditable='true'>${object.body}</p>
@@ -39,30 +117,39 @@ function printIdea() {
 					<h3>quality:<h4 class="quality">${object.quality}</h4></h3>
 				</article>
 				<hr>
-			</div>`);
-    });
+			</div>`
+    );
+  });
 }
 
 function clearInput() {
-    $('#title-input').val('');
-    $('#content-input').val('');
+  $('#title-input').val('');
+  $('#content-input').val('');
+}
+
+function disableEnter() {
+  if ($("#title-input").val() == "" || $("#content-input").val() == "") {
+    $("#submit").prop("disabled", true);
+  } else {
+    $("#submit").prop("disabled", false);
+  }
 }
 
 function editQuality(location, qualityVar) {
-    var objectId = $(location).parent().parent().attr("id");
-    data = JSON.parse(localStorage.getItem("Data Item"));
-    data.forEach(function(object) {
-        if (object.id == objectId) {
-            object.quality = qualityVar;
-            return object.quality;
-        }
-    });
-    stringData = JSON.stringify(data);
-    localStorage.setItem("Data Item", stringData);
+  var objectId = $(location).closest('.new-idea').attr('id');
+  data = JSON.parse(localStorage.getItem("Data Item"));
+  data.forEach(function(object) {
+    if (object.id == objectId) {
+      object.quality = qualityVar;
+      return object.quality;
+    }
+  });
+  stringData = JSON.stringify(data);
+  localStorage.setItem("Data Item", stringData);
 }
 
 function editTitleText(location, newText) {
-    var objectId = $(location).parent().parent().attr('id');
+    var objectId = $(location).closest('.new-idea').attr('id');
     data = JSON.parse(localStorage.getItem('Data Item'));
     data.forEach(function(object) {
         if (object.id == objectId) {
@@ -75,7 +162,7 @@ function editTitleText(location, newText) {
 }
 
 function editBodyText(location, newText) {
-    var objectId = $(location).parent().parent().attr('id');
+    var objectId = $(location).closest('.new-idea').attr('id');
     data = JSON.parse(localStorage.getItem('Data Item'));
     data.forEach(function(object) {
         if (object.id == objectId) {
@@ -87,8 +174,8 @@ function editBodyText(location, newText) {
     localStorage.setItem("Data Item", stringData);
 }
 
-function clear(location, idOfRemoved) {
-    var objectId = $(location).parent().parent().attr("id");
+function deleteCard (location, idOfRemoved) {
+    var objectId = $(location).closest('.new-idea').attr('id');
     var removedId = idOfRemoved;
     data = JSON.parse(localStorage.getItem("Data Item"));
     data = data.filter(function(object) {
@@ -97,90 +184,3 @@ function clear(location, idOfRemoved) {
     stringData = JSON.stringify(data);
     localStorage.setItem("Data Item", stringData);
 }
-
-function disableEnter() {
-    if ($("#title-input").val().length > 0 && $("#content-input").val().length > 0) {
-        $("#submit").prop("disabled", false);
-    } else {
-        $("#submit").prop("disabled", true);
-    }
-}
-
-$("#title-input, #content-input").on("keyup", disableEnter);
-
-$("#submit").on('click', function(e) {
-    e.preventDefault();
-    var storeIdeaTitle = $('#title-input').val();
-    var storeIdeaContent = $('#content-input').val();
-    var card = new Card(storeIdeaTitle, storeIdeaContent);
-    data.unshift(card);
-    storeIdea();
-    printIdea();
-    clearInput();
-    disableEnter();
-})
-
-$("#card-section").on('click', '.upvote', function() {
-    var qualityVar = $(this).siblings(".quality").text();
-    if ($(this).siblings(".quality").text() === "swill") {
-        $(this).siblings(".quality").text("plausible");
-        qualityVar = "plausible";
-        editQuality(this, qualityVar);
-    } else if ($(this).siblings(".quality").text() === "plausible") {
-        $(this).siblings(".quality").text("genius");
-        qualityVar = "genius"
-        editQuality(this, qualityVar);
-    } else if ($(this).siblings(".quality").text() === "genius") {
-        qualityVar = "genius";
-    }
-});
-
-$("#card-section").on('click', '.downvote', function() {
-    var qualityVar = $(this).siblings(".quality").text();
-    if ($(this).siblings(".quality").text() === "genius") {
-        $(this).siblings(".quality").text("plausible");
-        qualityVar = "plausible";
-        editQuality(this, qualityVar);
-    } else if ($(this).siblings(".quality").text() === "plausible") {
-        $(this).siblings(".quality").text("swill");
-        qualityVar = "swill";
-        editQuality(this, qualityVar);
-    } else if ($(this).siblings(".quality").text() === "plausible") {
-        qualityVar = "swill";
-    }
-});
-
-$('#card-section').on('blur', '.entry-title', function(e) {
-    var newTitleText = $(this).text();
-    editTitleText(this, newTitleText);
-});
-
-$('#card-section').on('blur', '.entry-body', function() {
-    var newBodyText = $(this).text();
-    editBodyText(this, newBodyText);
-});
-
-
-$("#card-section").on('click', '.clear', function() {
-    var idOfRemoved = $(this).parent().parent().attr("id")
-    clear(this, idOfRemoved);
-    $(this).closest('.new-idea').remove();
-});
-
-
-$('#search').on('keyup', function() {
-    var searchInput = $('#search').val();
-    var re = new RegExp(searchInput, 'igm');
-    $('.new-idea').each(function() {
-        var title = $(this).find(".entry-title").text();
-        var body = $(this).find("article p").text();
-        var match = (title.match(re) || body.match(re));
-        if (!match) {
-            $(this).hide();
-        } else {
-            $(this).show();
-        }
-    })
-});
-
-$("#title-input, #content-input").on("keyup", disableEnter);
